5fed9f15bdbccda3b150aedebfb5d67de6aa7348
chandana-tech <scb4java@yahoo.com>
2020-10-10 09:51:09 +0530

    added model classes

---
+++ src/main/java/chand/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,30 @@
+package chand.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDated;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/chand/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package chand.springframework.msscbeerservice.web.model;
+
+/**
+ *
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT,PORTER,ALE,WHEAT, IPA,SAISON
+}
