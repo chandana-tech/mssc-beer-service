f9c1d938cf45f205d744374cb56fb5aa44d46db8
chandana-tech <scb4java@yahoo.com>
2020-10-15 13:00:50 +0530

    added CRUD operations

---
+++ src/main/java/chand/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,28 @@
+package chand.springframework.msscbeerservice.web.controller;
+
+import chand.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @RequestMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto bearDto) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping("/{beerId}")
+    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
